a465e42bbe32fbcdf4cb5330ea77996f669dd788
heliont <heliomelont@gmail.com>
2024-10-01 22:35:43 -0300

    List genres endpoint api

--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from genres.models import Genre
 
 # Create your views here.
+
+def genre_view(request):
+    genres = Genre.objects.all()
+
+    # Serialize to work
+    #data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+
+    # List of genres in get endpoint
+    data = []
+    for genre in genres:
+        data.append({
+            'id': genre.id,
+            'name': genre.name
+        })
+
+    return JsonResponse(data, safe=False)
