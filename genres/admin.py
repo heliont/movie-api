4efb82b8e4d38f957f5c1d9fbfcef3133526b55a
heliont <heliomelont@gmail.com>
2024-10-01 22:13:14 -0300

    Create model e register admin

--- genres/admin.py
+++ genres/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Genre
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
+    list_display = ('id', 'name',)
