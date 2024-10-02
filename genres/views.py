c41a611d07975d4a7472d83bbc1cd6e0e30abe75
heliont <heliomelont@gmail.com>
2024-10-02 09:47:30 -0300

    Create genres via json

--- genres/views.py
+++ genres/views.py
@@ -1,20 +1,39 @@
+import json
 from django.http import JsonResponse
 from genres.models import Genre
 
+# O mesmo que acontece no form do template html para proteger o envio de dados
+from django.views.decorators.csrf import csrf_exempt
+
+
 # Create your views here.
+@csrf_exempt
+def genre_create_list_view(request):
 
-def genre_view(request):
-    genres = Genre.objects.all()
+    if request.method == 'GET':
+        genres = Genre.objects.all()
 
-    # Serialize to work
-    #data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+        # Serialize to work
+        #data = [{'id': genre.id, 'name': genre.name} for genre in genres]
 
-    # List of genres in get endpoint
-    data = []
-    for genre in genres:
-        data.append({
-            'id': genre.id,
-            'name': genre.name
-        })
+        # List of genres in get endpoint
+        data = []
+        for genre in genres:
+            data.append({
+                'id': genre.id,
+                'name': genre.name
+            })
+        return JsonResponse(data, safe=False)
 
-    return JsonResponse(data, safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data['name'])
+        new_genre.save()
+
+        return JsonResponse(
+            {
+                'id': new_genre.id,
+                'name': new_genre.name
+            }, 
+            status_code=201,
+        )
