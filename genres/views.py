a7f314ff17167be14400a262952be9a5618f4e25
heliont <heliomelont@gmail.com>
2024-10-11 23:31:49 -0300

    Crud completo via serializers Api

--- genres/views.py
+++ genres/views.py
@@ -1,14 +1,12 @@
 import json
 from django.http import JsonResponse
-from genres.models import Genre
 
 # Importando rest framwork
 from rest_framework import generics
-from genres.serializers import GenreSerializer
 
-# O mesmo que acontece no form do template html para proteger o envio de dados
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
+# Importando o modelo Genre do app genres
+from genres.models import Genre
+from genres.serializers import GenreSerializer
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
@@ -16,32 +14,14 @@ class GenreCreateListView(generics.ListCreateAPIView):
     serializer_class = GenreSerializer
 
 
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
 
-@csrf_exempt
-def genre_detail_view(request, pk):
-    genre = get_object_or_404(Genre, pk=pk)
-
-    if request.method == 'GET':
-
-        data = {'id': genre.id, 'name': genre.name}
-        return JsonResponse(data)
-    
-    elif request.method == 'PUT':
-        data = json.loads(request.body.decode('utf-8'))
-        genre.name = data['name']
-        genre.save()
-
-        return JsonResponse(
-            {
-                'id': genre.id,
-                'name': genre.name
-            },
-            status_code=200,
-        )
-
-    elif request.method == 'DELETE':
-        genre.delete()
-        return JsonResponse(
-            { 'message': 'Gênero excluído com sucesso.'}
-            #status_code=204,
-        )
+    def delete(self, *args, **kwargs):
+        instance = self.get_object()
+        try:
+            instance.delete()
+            return JsonResponse({'message': 'Genre deletado com sucesso.'}, status=204)
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500) 
