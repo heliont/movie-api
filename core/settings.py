62d9003c91c39d6cbe34367d9664340ada2b1ba8
heliont <heliomelont@gmail.com>
2024-11-04 23:55:50 -0300

    Configuração - Lifetime - token | API JWT

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,4 +141,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+# Configurações do Simple JWT
+SIMPLE_JWT = {
+    # Tempor máximo expirar token de accesso
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    # Tempor máximo expirar refresh token (renovar token de acesso)
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
