1e8fc36355c1bf8bb22d95ce8eaa2e81a2619314
heliont <heliomelont@gmail.com>
2024-11-04 23:39:53 -0300

    Rotas de refresh e verify API | Authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
+    # Geração de token de acesso para usuário
     path('authentication/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # Renova o token de acesso
+    path('authentication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Verifica o token de acesso é inválido ou expirado
+    path('authentication/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
